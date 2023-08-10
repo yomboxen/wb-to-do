4e444c5288f1fe6d0e656333b67c6111ff4b76ae
Luke Hardy <lukeclaytonhardy@gmail.com>
2023-08-10 09:43:23 -0600

    working on the to-do list

---
+++ index.js
@@ -0,0 +1,8 @@
+const addTodo = (event) => {
+    const newLi = document.createElement("li")
+    const input = document.getElementById("item")
+    item.innerText = input.value 
+
+    const list = document.querySelector('ul')
+    list.appendChild(item)
+}
