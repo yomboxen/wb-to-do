f924e5b50df9f91c1252ecc1dab755b2503590b4
Luke Hardy <lukeclaytonhardy@gmail.com>
2023-08-10 20:49:57 -0600

    completed the To-Do List code along

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const addTodo = (event) => {
-    const newLi = document.createElement("li")
-    const input = document.getElementById("item")
-    item.innerText = input.value 
+function completeTodo(event) {
+    const value = event.target.getAttribute('aria-checked')
+    if(value !== 'true') {
+        event.target.setAttribute('aria-checked', 'true')
+    } else {
+        event.target.setAttribute('aria-checked', 'false')
+    }
+}
+
+function removeTodo(event) {
+    event.target.parentNode.remove()
+}
+
+function addTodo(event) {
+    event.preventDefault();
+    const item = document.createElement("li");
+    item.innerText = document.querySelector("#item").value;
+    document.querySelector('click', completeTodo)
+    
+    const button = document.createElement('button');
+    button.innerText = 'X'
+    button.addEventListener('click', removeTodo)
+    item.append(button)
 
     const list = document.querySelector('ul')
     list.appendChild(item)
 }
+
+document.querySelector("form").addEventListener('submit', addTodo)
